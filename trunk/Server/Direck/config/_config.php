3490021d55054398ef410be243da804bd10f81aa
phamliem23@gmail.com <phamliem23@gmail.com@c54addbe-a1e8-9cac-5420-2b77249f9310>
2013-12-21 05:50:30 +0000

    function Check Token

--- trunk/Server/Direck/config/_config.php
+++ trunk/Server/Direck/config/_config.php
@@ -3,6 +3,9 @@ $config['db_host']	= 'localhost';
 $config['db_name'] 	= 'vnponcom_direck';
 $config['db_user']	= 'vnponcom_admin';
 $config['db_pass']	= 'E7k6T9nt';
+
+define("CHECK_TOKEN", false);
+define("MD5_KEY", "key@Direk");
 $tb_prefix		= 'direck_';
 
 include('_dbconnect.php');
